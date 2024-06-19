322160882f50cfabfba8deacf58790ffd13db38d
deelight-del <akingbenidavidoyin@gmail.com>
2024-06-19 16:59:18 +0000

    [task 3] trying possible fix

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -16,17 +16,21 @@ import redisClient from '../utils/redis';
 async function postNew(req, res) {
   const { email } = req.body;
   const { password } = req.body;
-  if (!email) {
+  if (email === undefined) {
     res.status(400).json({ error: 'Missing email' });
     return;
   }
-  if (!password) {
+  if (password === undefined) {
     res.status(400).json({ error: 'Missing password' });
     return;
   }
   const user = await dbClient.findUser(email);
   // console.log('This is the user', user);
-  if (user) {
+  if (user === undefined) {
+    res.json();
+    return;
+  }
+  if (user !== false) {
     res.status(400).json({ error: 'Already exist' });
     return;
   }
